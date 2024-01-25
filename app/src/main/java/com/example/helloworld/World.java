e0a876c918ec4b24c748bb7d8f7f5eb69ca3ebb4
Beck <becksons@bu.edu>
2024-01-25 14:15:29 -0500

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/World.java
@@ -0,0 +1,53 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.bumptech.glide.Glide;
+import com.example.helloworld.databinding.FragmentSecondBinding;
+
+
+public class World extends Fragment {
+    private FragmentSecondBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(World.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+
+        ImageView imageView = view.findViewById(R.id.myGifImageView);
+        Glide.with(this).asGif().load(R.drawable.helloturtle).into(imageView); // replace 'your_gif_name' with your GIF file name
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
